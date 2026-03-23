ae27233e9fa02a4e4b8232e3161a9c87ca014779
ENSfanaccount <enscdn@gmail.com>
2026-03-23 16:35:59 -0600

    se agregan las vistas

--- Sistema-tickets/routes/web.php
+++ Sistema-tickets/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TicketWebController;
 Route::get('/', function () {
-    return view('welcome');
+ return redirect()->route('tickets.index');
 });
+Route::resource('tickets', TicketWebController::class);
